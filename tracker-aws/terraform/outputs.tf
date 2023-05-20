01a8f7c490fe144435abad15e8f1d1ee265c6ee8
relyq <relyqx@gmail.com>
2023-05-19 23:04:31 -0300

    removed wrong lifecycle from outputs

--- tracker-aws/terraform/outputs.tf
+++ tracker-aws/terraform/outputs.tf
@@ -1,15 +1,7 @@
 output "api-public_ip" {
   value = aws_instance.api_server.public_ip
-
-  lifecycle {
-    replace_triggered_by = [ aws_instance.api_server.public_ip ]
-  }
 }
 
 output "frontend-public_ip" {
   value = aws_instance.frontend_server.public_ip
-
-  lifecycle {
-    replace_triggered_by = [ aws_instance.frontend_server.public_ip ]
-  }
 }
